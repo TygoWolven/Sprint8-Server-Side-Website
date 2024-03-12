d0700cf6ea6a69cdc1d1254baa9d4eef91a01889
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-03-12 23:57:54 +0100

    .

--- public/scripts/script.js
+++
@@ -1,9 +0,0 @@
-const navButton = document.getElementById('navigationButton'),
-      navBar = document.querySelector('.primary-navigation'),
-      buttonChevron = document.getElementById('buttonChevron');
-
-navButton.addEventListener('click', openNav)
-function openNav () {
-    navBar.classList.toggle('openNav')
-    buttonChevron.classList.toggle('rotateChevron')
-}
